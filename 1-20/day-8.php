2c24cafec16721b8c581678f7b77125a67c25141
barrow738 <57837415+barrow738@users.noreply.github.com>
2021-09-02 22:15:52 +0300

    Just watching video tutorial

---
+++ 1-20/day-8.php
@@ -0,0 +1,21 @@
+<?php 
+    $page_title = "Just watching video tutorial, not creating";
+    include "../header.php";
+    include "../header-menu.php";
+?>
+<main>
+    <section class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                <h1>
+                    <?php echo $page_title;?> ( <span class="date">September 2 <sup>th</sup>, 2021</span> )
+                </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<script>
+</script>
+<?php include "../footer.php";?>
