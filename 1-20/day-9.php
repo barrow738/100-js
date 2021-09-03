5236f77e46a829a9f1d3c472d761cfe0a16df30d
barrow738 <57837415+barrow738@users.noreply.github.com>
2021-09-03 22:01:27 +0300

    Just watcing

--- 1-20/day-9.php
+++ 1-20/day-9.php
@@ -1,5 +1,5 @@
 <?php 
-    $page_title = "Just watching video tutorial, not creating";
+    $page_title = "Only watching video tutorial";
     include "../header.php";
     include "../header-menu.php";
 ?>
@@ -9,10 +9,8 @@
             <div class="row">
                 <div class="col-md-12">
                 <h1>
-                    <?php echo $page_title;?> ( <span class="date">September 2 <sup>nd</sup>, 2021</span> )
+                    <?php echo $page_title;?> ( <span class="date">September 3 <sup>rd</sup>, 2021</span> )
                 </h1>
-                <p>onchange=""</p>
-                <p>getElemetsByTagName('button')</p>
                 </div>
             </div>
         </div>
