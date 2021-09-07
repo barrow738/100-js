b3faced31ad927837ec9ae9779d14e767dd996f9
barrow738 <57837415+barrow738@users.noreply.github.com>
2021-09-07 22:40:05 +0300

    Preventing default

--- 1-20/day-11.php
+++ 1-20/day-11.php
@@ -1,5 +1,5 @@
 <?php 
-    $page_title = "Just Watching tutorial";
+    $page_title = "Creating a Todo list";
     include "../header.php";
     include "../header-menu.php";
 ?>
@@ -11,11 +11,30 @@
                 <h1>
                     <?php echo $page_title;?> ( <span class="date">September 7 <sup>th</sup>, 2021</span> )
                 </h1>
+                <form action="">
+                    <input type="text" class="todo-input">
+                    <button type="submit" class="todo-btn btn btn-main">+</button>
+                </form>
+                <div class="todo-container">
+                    <ul class="todo-list">
+                        <li></li>
+                    </ul>
+                </div>
                 </div>
             </div>
         </div>
     </section>
 </main>
 <script>
+    const todoInput = document.querySelector('.todo-input')
+    const todoBtn = document.querySelector('.todo-btn')
+    const todoList = document.querySelector('.todo-list')
+
+    todoBtn.addEventListener('click', addTodo)
+
+    function addTodo(event) {
+        event.preventDefault()
+        alert('I think it is working now')
+    }
 </script>
 <?php include "../footer.php";?>
