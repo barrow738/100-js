ab966f8565099b932322e84b1800fb9fa85f777d
barrow738 <57837415+barrow738@users.noreply.github.com>
2021-09-13 21:18:00 +0300

    Watching CS50 tutorial

---
+++ 1-20/day-13.php
@@ -0,0 +1,21 @@
+<?php 
+    $page_title = "Watching CS50's Javascript tutorial";
+    include "../header.php";
+    include "../header-menu.php";
+?>
+<main>
+    <section class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                <h1>
+                    <?php echo $page_title;?> ( <span class="date">September 13 <sup>th</sup>, 2021</span> )
+                </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<script>
+</script>
+<?php include "../footer.php";?>
